0503bd232b07b00f148f2c1edda888d5cca4b108
Luke Paoloni <lpaoloni@de-facto.com>
2021-12-21 19:34:54 +0000

    ...

--- git-sync.php
+++ git-sync.php
@@ -17,44 +17,57 @@ if ( ! defined('ABSPATH') ) {
     exit;
 }
 
+if ( is_readable( __DIR__ . '/vendor/autoload.php' ) ) {
+    require __DIR__ . '/vendor/autoload.php';
+}
+
 /**
  *
- * @param object $wp_upgrader Plugin_Upgrader instance.
- * @param array  $hook_extra  {
- *     Array of bulk item update data.
- *
- *     @type string $action  Type of action. Default 'update'.
- *     @type string $type    Type of update process. Accepts 'plugin', 'theme', 'translation', or 'core'.
- *     @type bool   $bulk    Whether the update process is a bulk update. Default true.
- *     @type array  $plugins Array of the basename paths of the plugins' main files.
- * }
+ * @param bool|WP_Error $return Upgrade offer return.
+ * @param array         $plugin Plugin package arguments.
  */
-function rhgp_handle_other_plugins_update( $wp_upgrader, $hook_extra ) {
-    if ( ! isset( $hook_extra['action'], $hook_extra['type'], $hook_extra['plugins'] ) ) {
-		return;
-	}
+function rhgp_handle_plugin_update( $return, $plugin ) {
 
-    if ( 'update' !== $hook_extra['action'] || 'plugin' !== $hook_extra['type'] || ! is_array( $hook_extra['plugins'] ) ) {
-		return;
-	}
-
-    if ( ! defined('GIT_REPOSITORY_URL') ) {
-        return;
+    if ( ! defined('GIT_REPOSITORY_PATH') ) {
+        return $return;
     }
 
-    $plugins = $hook_extra['plugins'];
+    if ( ! defined('GIT_SC_PLUGINS') ) {
+        return $return;
+    }
+
+    if ( ! isset($plugin['plugin']) ) {
+        return $return;
+    }
+
+    $allowedPluginsInSc = array_map('trim', explode(',', GIT_SC_PLUGINS));
+    $plugin = preg_replace('/\/.+/', '', $plugin['plugin']);
+
+    if ( ! in_array($plugin, $allowedPluginsInSc) ) {
+        return $return;
+    }
 
     // 1. Is plugin allowed to push to source countrol?
         // a. Is plugin flagged to be pushed to a source control (e.g. not a plugin that should be updated via composer)
         // b. Has the current user got write permissions
     // 2. Stage the change of the plugin update
-    $git = new CzProject\GitPhp\Git;
-    $repo = $git->open(GIT_REPOSITORY_URL);
+    $git = new CzProject\GitPhp\Git();
+    $repo = $git->open(GIT_REPOSITORY_PATH);
+
+    if ( ! $repo->hasChanges() ) {
+        return $return;
+    }
+
+    $plugin_abs_file = plugin_dir_path(__FILE__ . '../' . $plugin['plugin']);
+    $plugin_data = get_plugin_data($plugin_abs_file, false, false);
+
     $repo->addAllChanges();
     // 3. Create a feature branch to push the changes to
-    $repo->createBranch(md5(time()), TRUE);
+    $branch = 'feature/upgrade/' . $plugin . '-' . md5(time());
+    $repo->createBranch($branch, TRUE);
     // 4. Commit with a generated message
     $repo->commit('commit message');
     $repo->push('origin');
+    return $return;
 }
-add_action( 'upgrader_process_complete', 'rhgp_handle_other_plugins_update', 25, 2 );
+add_filter( 'upgrader_post_install', 'rhgp_handle_plugin_update', 10, 2 );

2717c47cadbba72b863093a3bd43674cf5288cc5
Luke Paoloni <lukepaoloni.me@gmail.com>
2021-12-22 06:33:51 +0000

    ...

--- git-sync.php
+++ git-sync.php
@@ -19,6 +19,8 @@ if ( ! defined('ABSPATH') ) {
 
 if ( is_readable( __DIR__ . '/vendor/autoload.php' ) ) {
     require __DIR__ . '/vendor/autoload.php';
+} else {
+    exit;
 }
 
 /**
@@ -28,11 +30,11 @@ if ( is_readable( __DIR__ . '/vendor/autoload.php' ) ) {
  */
 function rhgp_handle_plugin_update( $return, $plugin ) {
 
-    if ( ! defined('GIT_REPOSITORY_PATH') ) {
+    if ( ! defined('GIT_REPOSITORY_PATH') || ( defined('GIT_REPOSITORY_PATH') && !GIT_REPOSITORY_PATH ) ) {
         return $return;
     }
 
-    if ( ! defined('GIT_SC_PLUGINS') ) {
+    if ( ! defined('GIT_SC_PLUGINS') || ( defined('GIT_SC_PLUGINS') && !GIT_SC_PLUGINS ) ) {
         return $return;
     }
 
@@ -41,33 +43,46 @@ function rhgp_handle_plugin_update( $return, $plugin ) {
     }
 
     $allowedPluginsInSc = array_map('trim', explode(',', GIT_SC_PLUGINS));
-    $plugin = preg_replace('/\/.+/', '', $plugin['plugin']);
+    $pluginName = preg_replace('/\/.+/', '', $plugin['plugin']);
 
-    if ( ! in_array($plugin, $allowedPluginsInSc) ) {
+    if ( ! in_array($pluginName, $allowedPluginsInSc) ) {
         return $return;
     }
 
-    // 1. Is plugin allowed to push to source countrol?
-        // a. Is plugin flagged to be pushed to a source control (e.g. not a plugin that should be updated via composer)
-        // b. Has the current user got write permissions
-    // 2. Stage the change of the plugin update
-    $git = new CzProject\GitPhp\Git();
-    $repo = $git->open(GIT_REPOSITORY_PATH);
+    try {
+        $git = new \CzProject\GitPhp\Git();
+        $repo = $git->open(GIT_REPOSITORY_PATH);
+        $currentBranch = $repo->getCurrentBranchName();
 
-    if ( ! $repo->hasChanges() ) {
-        return $return;
+        if ( ! $repo->hasChanges() ) {
+            return $return;
+        }
+
+        $plugins_abs_folder = WP_CONTENT_DIR . '/plugins/';
+        $plugin_data = get_plugin_data($plugins_abs_folder . $plugin['plugin'], false, false);
+        $repo->addFile($plugins_abs_folder . $pluginName);
+        $tag = $pluginName . '-' . $plugin_data['Version'];
+        try {
+            $repo->createTag($tag, TRUE);
+            $commitMessage = 'GitPush: Upgrade ' . $pluginName . ' to version ' . $plugin_data['Version'] . '.';
+            if ($user = wp_get_current_user()) {
+                $commitMessage = '['.$user->user_login.'] ' . $commitMessage;
+            }
+            $repo->commit($commitMessage);
+            $repo->push('origin', ['--tags']);
+        } catch (\Exception $ex2) {
+            if ($repo->getCurrentBranchName() !== $currentBranch) {
+                $brokenBranch = $repo->getCurrentBranchName();
+                if ($repo->hasChanges()) {
+                    $repo->execute('checkout .');
+                }
+                $repo->checkout($currentBranch);
+                $repo->removeBranch($brokenBranch);
+            }
+        }
+    } catch (\Exception $ex) {
+        throw $ex;
     }
-
-    $plugin_abs_file = plugin_dir_path(__FILE__ . '../' . $plugin['plugin']);
-    $plugin_data = get_plugin_data($plugin_abs_file, false, false);
-
-    $repo->addAllChanges();
-    // 3. Create a feature branch to push the changes to
-    $branch = 'feature/upgrade/' . $plugin . '-' . md5(time());
-    $repo->createBranch($branch, TRUE);
-    // 4. Commit with a generated message
-    $repo->commit('commit message');
-    $repo->push('origin');
     return $return;
 }
 add_filter( 'upgrader_post_install', 'rhgp_handle_plugin_update', 10, 2 );
